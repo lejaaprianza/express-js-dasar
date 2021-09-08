e4df3d23b95077d624a1e384ed4afa0cded3df91
lejaaprianza <lejaprianza@gmail.com>
2021-09-08 10:26:49 +0700

    tes

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var app = express();
+var express = require("express")
+var app = express()
 
-app.get('/', function(req, res){
-   res.send("Lampung Kode");
-});
+app.get("/", function (req, res) {
+  res.send("Lampung Kode")
+})
 
-app.listen(3000);
+app.listen(4000)
