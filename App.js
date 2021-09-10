429f881dad88033ee1baf03e8c2fe528a8a802ff
lejaaprianza <lejaprianza@gmail.com>
2021-09-10 08:16:34 +0700

    tutorial 2

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 var express = require("express")
 var app = express()
 
-app.get("/", function (req, res) {
-  res.send("Lampung Kode")
+app.get("/hello", function (req, res) {
+  res.send("Hello World!")
 })
 
-app.listen(4000)
+app.post("/hello", function (req, res) {
+  res.send("You just called the post method at '/hello'!\n")
+})
+
+app.listen(3000)
