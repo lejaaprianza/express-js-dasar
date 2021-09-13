59283089df2bb7c5f8454fcd50a300af1131df72
lejaaprianza <lejaprianza@gmail.com>
2021-09-13 09:34:06 +0700

    tutorial-2

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ var express = require("express")
 var app = express()
 
 app.get("/hello", function (req, res) {
-  res.send("Hello World!")
+  res.send("Lampung Kode")
 })
 
 app.post("/hello", function (req, res) {
-  res.send("You just called the post method at '/hello'!\n")
+  res.send("Selamat datang di lampung kode!")
 })
 
-app.listen(3000)
+app.all("/test", function (req, res) {
+  res.send("method post dan method get bisa dilakukan pada route ini")
+})
+
+app.listen(4000)
