86e73f8358aac7c7f602d7a4a7debb571378137f
lejaaprianza <lejaprianza@gmail.com>
2021-09-13 10:06:54 +0700

    tutorial 3

--- App.js
+++ App.js
@@ -1,16 +1,8 @@
-var express = require("express")
+var express = require("Express")
 var app = express()
 
-app.get("/hello", function (req, res) {
-  res.send("Lampung Kode")
-})
+var lampungKode = require("./lampungKode.js")
 
-app.post("/hello", function (req, res) {
-  res.send("Selamat datang di lampung kode!")
-})
-
-app.all("/test", function (req, res) {
-  res.send("method post dan method get bisa dilakukan pada route ini")
-})
+app.use("/lampungkode", lampungKode)
 
 app.listen(4000)
