0434bde50444d52bb8f0bf16408c42c9197d066f
lejaaprianza <lejaprianza@gmail.com>
2021-09-17 08:16:52 +0700

    tutorial 5

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
-var express = require("Express")
+var express = require("express")
 var app = express()
 
-var lampungKode = require("./lampungKode.js")
+app.get("/:id", function (req, res) {
+  res.send("ID kamu adalah " + req.params.id)
+})
 
-app.use("/lampungkode", lampungKode)
+app.get("/lampung/:nama/:id", function (req, res) {
+  res.send("id: " + req.params.id + " dan nama: " + req.params.nama)
+})
 
 app.listen(4000)
