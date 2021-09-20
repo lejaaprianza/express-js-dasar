3da5d89e26865f7f820a9f8e9f200da2743d7843
lejaaprianza <lejaprianza@gmail.com>
2021-09-20 08:43:38 +0700

    tutorial 6

--- App.js
+++ App.js
@@ -1,12 +1,8 @@
 var express = require("express")
 var app = express()
 
-app.get("/:id", function (req, res) {
-  res.send("ID kamu adalah " + req.params.id)
-})
-
-app.get("/lampung/:nama/:id", function (req, res) {
-  res.send("id: " + req.params.id + " dan nama: " + req.params.nama)
+app.get("/kodepos/:kode([0-9]{5})", function (req, res) {
+  res.send("Kode Pos: " + req.params.kode)
 })
 
 app.listen(4000)
