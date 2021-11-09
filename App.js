735f714f3d94aaea3342096be2fc53ac85a71203
lejaaprianza <lejaprianza@gmail.com>
2021-11-09 10:52:19 +0700

    tutorial 11

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 var express = require("express")
 var app = express()
 
-app.get("/user/:id", function (req, res, next) {
-  res.send("method GET Lampung Kode")
-})
-
-app.get("/user/:id", function (req, res, next) {
-  res.send("method GET yang kedua Lampung Kode")
-})
-
-app.post("/user/:id", function (req, res, next) {
-  res.send("method POST Lampung Kode")
-})
+app.use(
+  "/user/:id",
+  function (req, res, next) {
+    console.log("Request URL:", req.originalUrl)
+    next()
+  },
+  function (req, res, next) {
+    console.log("Request Type:", req.method)
+    next()
+  }
+)
 
 app.listen(4000)
