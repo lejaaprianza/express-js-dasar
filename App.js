fdf680c053da8a3e804247c069fd4f34d001d92c
lejaaprianza <lejaprianza@gmail.com>
2021-11-10 10:47:09 +0700

    tutorial 12

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 var express = require("express")
 var app = express()
 
-app.use(
+app.get(
   "/user/:id",
   function (req, res, next) {
-    console.log("Request URL:", req.originalUrl)
-    next()
+    if (req.params.id === "0") next("route")
+    else next()
   },
   function (req, res, next) {
-    console.log("Request Type:", req.method)
-    next()
+    res.send("Halaman ini muncul kalau params tidak sama dengan 0")
   }
 )
 
+app.get("/user/:id", function (req, res, next) {
+  res.send("Params sama dengan 0")
+})
 app.listen(4000)
