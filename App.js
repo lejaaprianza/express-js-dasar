179ce2b0cc636040391319d22512f3b55ab20e81
lejaaprianza <lejaprianza@gmail.com>
2021-11-13 15:18:04 +0700

    tutorial 14

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
 var express = require("express")
 var app = express()
+var router = express.Router()
 
-function logMethod(req, res) {
-  res.send("Tipe Request-nya: " + req.method)
-}
+router.use(function (req, res, next) {
+  console.log("Time:", Date.now())
+  next()
+})
 
-app.get("/user/:id", logMethod)
-app.post("/user/:id", logMethod)
+router.use(
+  "/user/:id",
+  function (req, res, next) {
+    console.log("Request URL:", req.originalUrl)
+    next()
+  },
+  function (req, res, next) {
+    console.log("Request Type:", req.method)
+    next()
+  }
+)
+
+router.get(
+  "/user/:id",
+  function (req, res, next) {
+    if (req.params.id === "0") next("route")
+    else next()
+  },
+  function (req, res, next) {
+    res.send("ketika parameter id != 0")
+  }
+)
+
+router.get("/user/:id", function (req, res, next) {
+  console.log(req.params.id)
+  res.send("ketika parameter id = 0")
+})
+
+app.use("/", router)
 
 app.listen(4000)
