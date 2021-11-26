1d0964bd20ff23a78de4b0eca0ae440e91c59d9b
lejaaprianza <lejaprianza@gmail.com>
2021-11-26 21:09:20 +0700

    tutorial 15

--- App.js
+++ App.js
@@ -1,40 +1,16 @@
 var express = require("express")
 var app = express()
-var router = express.Router()
 
-router.use(function (req, res, next) {
-  console.log("Time:", Date.now())
-  next()
+app.get("/", function (req, res) {
+  next(err)
 })
 
-router.use(
-  "/user/:id",
-  function (req, res, next) {
-    console.log("Request URL:", req.originalUrl)
-    next()
-  },
-  function (req, res, next) {
-    console.log("Request Type:", req.method)
-    next()
-  }
-)
-
-router.get(
-  "/user/:id",
-  function (req, res, next) {
-    if (req.params.id === "0") next("route")
-    else next()
-  },
-  function (req, res, next) {
-    res.send("ketika parameter id != 0")
-  }
-)
-
-router.get("/user/:id", function (req, res, next) {
-  console.log(req.params.id)
-  res.send("ketika parameter id = 0")
+app.post("/", function (req, res) {
+  res.send("tes")
 })
 
-app.use("/", router)
-
+app.use(function (err, req, res, next) {
+  res.status(500)
+  res.send("Wah, ada yang error...")
+})
 app.listen(4000)
